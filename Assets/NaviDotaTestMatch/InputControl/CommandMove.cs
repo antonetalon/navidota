d9d11486af03d9ecc66efef41e1eb68ce2ffaa78
AntonPendiur <a.pendiur@patrioticgames.org>
2016-12-23 17:49:27 +0200

    Amend last.

--- Assets/NaviDotaTestMatch/InputControl/CommandMove.cs
+++ Assets/NaviDotaTestMatch/InputControl/CommandMove.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 using GameSparks.RT;
 
 public class CommandMove : MatchCommand {
-	public CommandMove(Vector2 target):base(null) {}
+	public CommandMove(Vector2 target):base(null) {
+		Data = new RTData();
+		Data.SetVector2(0, target);
+	}
 }
